3c06d7666a4ad786484bfae933685ce2a5c3961a
blackhat_coder <ankit.21.kumar02@gmail.com>
2024-03-23 09:46:51 +0530

    initial commit

---
+++ app/src/main/java/com/example/parkease/onboardingScreen/OnboardingItemsAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.parkease.onboardingScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.parkease.R
+
+class OnboardingItemsAdapter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_items_adapter)
+    }
+}
