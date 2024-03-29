8b96d41cdcea5a2d94eb976001af000e3aca2845
blackhat_coder <ankit.21.kumar02@gmail.com>
2024-03-29 11:31:02 +0530

    Login/Register Updated

--- app/src/main/java/com/example/parkease/splashScreen/SplashScreen.kt
+++ app/src/main/java/com/example/parkease/splashScreen/SplashScreen.kt
@@ -1,12 +1,74 @@
 package com.example.parkease.splashScreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.parkease.HomeScreen.Home
+import com.example.parkease.MainActivity
 import com.example.parkease.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 class SplashScreen : AppCompatActivity() {
+
+    private var auth: FirebaseAuth = Firebase.auth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        auth = FirebaseAuth.getInstance()
+        supportActionBar?.hide()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        val executor = Executors.newSingleThreadScheduledExecutor()
+        executor.schedule({
+            Handler(Looper.getMainLooper()).post {
+                checkUser()
+            }
+        }, 3, TimeUnit.SECONDS)
+    }
+
+    private fun checkUser() {
+        val firebaseUser = auth.currentUser
+        if (firebaseUser != null) {
+            val ref = FirebaseDatabase.getInstance().getReference("Users")
+
+            ref.child(firebaseUser!!.uid).addListenerForSingleValueEvent(object :
+                ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val userType = snapshot.child("userType").getValue(String::class.java)
+
+                    if (userType == "user") {
+                        val intent = Intent(this@SplashScreen, Home::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+//                    else if(userType == "admin"){
+//                        val intent = Intent(this@SplashScreen, AdminActivity::class.java)
+//                        startActivity(intent)
+//                        finish()
+//                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // Handle onCancelled event
+                }
+            })
+        }
+        else{
+            val intent = Intent(this@SplashScreen, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
