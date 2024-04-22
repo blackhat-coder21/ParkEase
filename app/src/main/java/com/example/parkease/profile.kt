63674e60d57d8e25b7e84172c3bb3aa9b6480b52
Chinmay Tayade <97028462+chinmayindepay@users.noreply.github.com>
2024-04-22 13:29:13 +0530

    onboarding screens

--- app/src/main/java/com/example/parkease/profile.kt
+++ app/src/main/java/com/example/parkease/profile.kt
@@ -11,10 +11,10 @@ class profile : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 }
